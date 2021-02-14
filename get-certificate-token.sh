89efc1f2184c1c13dc39abd8187b24024bcf6511
valador <slayer.rus@gmail.com>
2021-02-14 20:13:44 +0000

    upd

--- get-certificate-token.sh
+++ get-certificate-token.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -x
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml
       
 grep 'certificate' $KUBECONFIG | awk -F ': ' '{print $2}' | base64 -d > ./secrets/CA.txt
 
-SECRET=$(kubectl -n kube-system get secret | grep gitlab | awk '{print $1}')
+SECRET=$(kubectl -n kube-system get secret | grep gitlab-admin | awk '{print $1}')
 
 kubectl -n kube-system get secret $SECRET -o jsonpath='{.data.token}' | base64 -d > ./secrets/TOKEN.txt
 
