d2b90777dcb401ce6f4846469bfd78ff0a88f58d
valador <slayer.rus@gmail.com>
2021-02-15 12:15:13 +0000

    add reg external url (SelfSignedCert)

--- deploy-kaniko-to-registry.sh
+++ deploy-kaniko-to-registry.sh
@@ -10,10 +10,10 @@ image_name_to_pull="gcr.io/kaniko-project/executor:debug"
 registry_domain="reg.gitlab.dev.lan"
 
 docker pull ${image_name_to_pull}
-docker tag ${image_name_to_pull} ${registry_domain}:5050/${image_name_to_pull}
-docker push ${registry_domain}:5050/${image_name_to_pull}
+docker tag ${image_name_to_pull} ${registry_domain}:5005/${image_name_to_pull}
+docker push ${registry_domain}:5005/${image_name_to_pull}
 
-curl https://${registry_domain}:5050/v2/_catalog
+curl https://${registry_domain}:5005/v2/_catalog
 #curl http://${registry_domain}:5000/v2/node/tags/list
 #curl http://registry.dev.test:5000/v2/gcr.io/kaniko-project/executor/tags/list
 
